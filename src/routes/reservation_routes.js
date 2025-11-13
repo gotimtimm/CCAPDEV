dc6112c53eb3079bf1b4dc4a058f752232ec3f70
VPangan <vince_pangan@dlsu.edu.ph>
2025-11-13 23:55:43 +0800

    reservation routes

--- src/routes/reservation_routes.js
+++ src/routes/reservation_routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Reservation = require('../models/reservation');
 const Flight = require('../models/flight');
+const User = require('../models/user');
 const { isAuthenticated } = require('../middleware/auth');
 
 router.get('/reservation-form', isAuthenticated, async (req, res) => {
